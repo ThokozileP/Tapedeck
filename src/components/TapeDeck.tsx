1ce2c66da55f530c81f7caeaaf46d3394b09218e
tphiri641_comcast <tphiri641@emea.comcast.com>
2023-06-24 12:53:49 +0200

    fix: refactored api fetching mechanism

--- src/components/TapeDeck.tsx
+++ src/components/TapeDeck.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useMemo } from "react";
 import {
   useTable,
   useGlobalFilter,
@@ -10,117 +10,13 @@ import { GlobalFilter } from "./GlobalFilter";
 import { COLUMNS } from "./Columns";
 import "./Table.css";
 import { ColumnFilter } from "./ColumnFilter";
-
-interface Tape {
-  page: string;
-  img: string;
-  thumb: string;
-  playingTime: string;
-  type?: string;
-  brand: string;
-  color?: string;
-}
-
-interface TapeData {
-  page: string;
-  img: string;
-  thumb: string;
-  playingTime: string;
-  type?: string;
-  brand?: string;
-  color?: string;
-}
-
-interface TapeItem {
-  [key: string]: TapeData[];
-}
+import {useApiGet} from "../services/tapedeck.service"
 
 const TapeDeck: React.FC = () => {
-  const [tapesFormated, setTapesFormated] = useState<Tape[]>([]);
-  const [tapes, setTapes] = useState<TapeItem[]>([]);
-
-  useEffect(() => {
-    const fetchTapes = async () => {
-      try {
-        const response = await fetch("https://tapedeck-api.vercel.app/api", {
-          headers: {
-            "x-api-key":
-              "QXJlIHlvdSBvdXIgbmV3IGZyb250LWVuZCBzb2Z0d2FyZSBlbmdpbmVlciwgVGhva296aWxlPw==",
-          },
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          setTapes(data);
-          const sortedValues = data.map((tape: { [x: string]: any }) => {
-            const tapeKey = Object.keys(tape);
-
-            const tapeData = tape[tapeKey[0]];
-
-            return tapeData;
-          });
-
-          const objects = sortedValues.map((tape: { [x: string]: any }) => {
-            let page = undefined;
-            let pageUrl = "";
-            let img = undefined;
-            let thumb = undefined;
-            let playingTime = "";
-            let type = "";
-            let color = "";
-            let brand = "";
-
-            tape.forEach((element: any) => {
-              if (element.hasOwnProperty("page")) {
-                const pagePath = element.page
-                pageUrl = pagePath
-                page = <a href={pagePath}>Link</a>
-              } else if (element.hasOwnProperty("img")) {
-                const imagePath = element.img;
-                img = <img src={imagePath} alt="" />;
-              } else if (element.hasOwnProperty("thumb")) {
-                const thumbPath = element.thumb;
-                thumb = <a href={pageUrl}><img src={thumbPath} alt="" /></a>;
-              } else if (element.hasOwnProperty("playingTime")) {
-                playingTime = element.playingTime;
-              } else if (element.hasOwnProperty("type")) {
-                type = element.type;
-              } else if (element.hasOwnProperty("color")) {
-                color = element.color;
-              } else if (element.hasOwnProperty("brand")) {
-                brand = element.brand;
-              }
-            });
-
-            return {
-              page: page,
-              img: img,
-              thumb: thumb,
-              playingTime: playingTime,
-              type: type,
-              color: color,
-              brand: brand,
-            };
-          });
-          const sortByBrand = [...objects].sort((a, b) => a.brand.localeCompare(b.brand));
-          setTapesFormated(sortByBrand);
-        } else {
-          console.error(
-            "Failed to fetch tapes:",
-            response.status,
-            response.statusText
-          );
-        }
-      } catch (error) {
-        console.error("Failed to fetch tapes:", error);
-      }
-    };
-
-    fetchTapes();
-  }, [tapes]);
-
   const columns = useMemo(() => COLUMNS, []);
-  const data: any = useMemo(() => tapesFormated, [tapesFormated]);
+  const apiUrl = "https://tapedeck-api.vercel.app/api";
+  const data: any = useApiGet(apiUrl)
+
   const defaultColumn: any = useMemo(() => {
     return {
       Filter: ColumnFilter,
